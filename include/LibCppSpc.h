db56e2449c416cbdc4f0758a8e29a047cd965227
Stephen Bonar <contactme@stephenbonar.com>
2025-06-25 12:49:24 -0700

    Add main library header file
    -Created LibCppSpc.h that includes all the other header files

---
+++ include/LibCppSpc.h
@@ -0,0 +1,46 @@
+// LibCppSpc.h - The main library header.
+//
+// Copyright (C) 2025 Stephen Bonar
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http ://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LIB_CPP_SPC_H
+#define LIB_CPP_SPC_H
+
+#include "BinaryField.h"
+#include "BinaryTag.h"
+#include "DataStructure.h"
+#include "DateField.h"
+#include "EmulatorField.h"
+#include "ExtendedTag.h"
+#include "ExtentedTagItem.h"
+#include "Field.h"
+#include "File.h"
+#include "FileStream.h"
+#include "Format.h"
+#include "Header.h"
+#include "NumericField.h"
+#include "NumericType.h"
+#include "SetCommand.h"
+#include "StringDelimiter.h"
+#include "StringSegment.h"
+#include "StringSegmentType.h"
+#include "StringToken.h"
+#include "StringTokenizer.h"
+#include "StringValueType.h"
+#include "TagType.h"
+#include "TextField.h"
+#include "TextTag.h"
+#include "TrackField.h"
+
+#endif
