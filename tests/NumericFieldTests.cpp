77b49e0bea4628441cd47f22713512345e81e6b4
Stephen Bonar <contactme@stephenbonar.com>
2026-02-22 14:20:14 -0700

    Completed NumericFieldTests
    -All tests passed
    -TODO: Finish writing tests for remaining types

--- tests/NumericFieldTests.cpp
+++ tests/NumericFieldTests.cpp
@@ -16,3 +16,213 @@
 
 #include "NumericFieldTests.h"
 
+void NumericFieldTests::SetUp()
+{
+    // No setup needed for these tests.
+}
+
+TEST_F(NumericFieldTests, EnsureSizeGreaterThanZero)
+{
+    Spc::FieldInfo info{ 4, 0 };
+    std::string label{ "Test Numeric Field" };
+    EXPECT_THROW(Spc::NumericField(label, info), std::invalid_argument);
+}
+
+TEST_F(NumericFieldTests, ConstructorsInitializesProperly)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info);
+    Spc::NumericField numericFieldWithType(label, info, Spc::NumericType::Text);
+
+    EXPECT_EQ(numericField.Label(), label);
+    EXPECT_EQ(numericField.Offset(), info.offset);
+    EXPECT_EQ(numericField.Size(), info.size);
+    EXPECT_EQ(numericFieldWithType.Label(), label);
+    EXPECT_EQ(numericFieldWithType.Offset(), info.offset);
+    EXPECT_EQ(numericFieldWithType.Size(), info.size);
+    EXPECT_EQ(numericFieldWithType.Type(), Spc::NumericType::Text);
+}
+
+TEST_F(NumericFieldTests, IsZeroReturnsTrueForAllZeroBytes)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    Spc::NumericField numericField("Test Numeric Field", info);
+    std::fill_n(numericField.RawData(), numericField.Size(), 0);
+
+    EXPECT_TRUE(numericField.IsZero());
+}
+
+TEST_F(NumericFieldTests, IsZeroReturnsFalseForNonZeroBytes)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    Spc::NumericField numericField("Test Numeric Field", info);
+    numericField.SetValue("1");
+
+    EXPECT_FALSE(numericField.IsZero());
+}
+
+TEST_F(NumericFieldTests, IsTextReturnsTrueForAsciiDigits)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Text);
+    numericField.SetValue("1234");
+
+    EXPECT_TRUE(numericField.IsText());
+}
+
+TEST_F(NumericFieldTests, IsTextReturnsFalseForNonAsciiDigits)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    numericField.SetValue("1234");
+
+    EXPECT_FALSE(numericField.IsText());
+}
+
+TEST_F(NumericFieldTests, DetectInt32ReturnsCorrectValueForTextType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Text);
+    
+    numericField.SetValue("1234");
+    numericField.SetType(Spc::NumericType::Either);
+
+    EXPECT_EQ(numericField.DetectInt32(), 1234);
+}
+
+TEST_F(NumericFieldTests, DetectInt32ReturnsCorrectValueForBinaryType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    
+    numericField.SetValue("1234");
+    numericField.SetType(Spc::NumericType::Either);
+
+    EXPECT_EQ(numericField.DetectInt32(), 1234);
+}
+
+TEST_F(NumericFieldTests, ToInt32ReturnsCorrectValue)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    
+    numericField.SetValue("-1234");
+
+    EXPECT_EQ(numericField.ToInt32(), -1234);
+}
+
+TEST_F(NumericFieldTests, ToUInt32ReturnsCorrectValue)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    
+    numericField.SetValue("1234");
+
+    EXPECT_EQ(numericField.ToUInt32(), 1234);
+}
+
+TEST_F(NumericFieldTests, ToStringReturnsCorrectValueForTextType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Text);
+    
+    numericField.SetValue("1234");
+
+    EXPECT_EQ(numericField.ToString(), "1234");
+}
+
+TEST_F(NumericFieldTests, ToStringReturnsCorrectValueForBinaryType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    
+    numericField.SetValue("1234");
+
+    EXPECT_EQ(numericField.ToString(), "1234");
+}
+
+TEST_F(NumericFieldTests, ToStringReturnsCorrectValueForEitherType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Either);
+    
+    numericField.SetValue("1234");
+
+    EXPECT_EQ(numericField.ToString(), "1234");
+}
+
+TEST_F(NumericFieldTests, SetInt32SetsValueCorrectly)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    
+    numericField.SetInt32(-1234);
+
+    EXPECT_EQ(numericField.ToInt32(), -1234);
+}
+
+TEST_F(NumericFieldTests, SetUInt32SetsValueCorrectly)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    
+    numericField.SetUInt32(1234);
+
+    EXPECT_EQ(numericField.ToUInt32(), 1234);
+}
+
+TEST_F(NumericFieldTests, SetValueSetsValueCorrectlyForTextType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Text);
+    
+    numericField.SetValue("1234");
+
+    EXPECT_EQ(numericField.Value(), "1234");
+}
+
+TEST_F(NumericFieldTests, SetValueSetsValueCorrectlyForBinaryType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Binary);
+    
+    numericField.SetValue("1234");
+
+    EXPECT_EQ(numericField.ToInt32(), 1234);
+}
+
+TEST_F(NumericFieldTests, SetValueSetsValueCorrectlyForEitherType)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Either);
+    
+    numericField.SetValue("1234");
+
+    EXPECT_EQ(numericField.Value(), "1234");
+}
+
+TEST_F(NumericFieldTests, SetsTypeCorrectly)
+{
+    Spc::FieldInfo info{ 4, 4 };
+    std::string label{ "Test Numeric Field" };
+    Spc::NumericField numericField(label, info, Spc::NumericType::Either);
+    
+    numericField.SetType(Spc::NumericType::Text);
+
+    EXPECT_EQ(numericField.Type(), Spc::NumericType::Text);
+}
